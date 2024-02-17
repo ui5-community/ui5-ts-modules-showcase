aa96c9812491f5b5b6bbeebff92b623ed5c18641
Peter Muessig <peter.muessig@sap.com>
2024-02-17 08:20:55 +0100

    Initial commit

---
+++ webapp/controller/App.controller.ts
@@ -0,0 +1,11 @@
+import BaseController from "./BaseController";
+
+/**
+ * @namespace my.tsapp.showcase.controller
+ */
+export default class App extends BaseController {
+	public onInit(): void {
+		// apply content density mode to root view
+		this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+	}
+}
